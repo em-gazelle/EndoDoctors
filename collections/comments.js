aad2df838224d076568a1f9573feaa4eed1f9502
em-gazelle <emmaf9@gmail.com>
2013-12-11 22:05:55 -0800

    "first version, prior"

---
+++ collections/comments.js
@@ -0,0 +1,32 @@
+Comments = new Meteor.Collection('comments');
+
+//creating the Meteor Method to post new comments:
+
+Meteor.methods({
+	comment: function(commentAttributes) {
+		var user = Meteor.user();
+		var post = Posts.findOne(commentAttributes.postId);
+
+		if (!user)
+			{throw new Meteor.Error(401, "Please login to submit a review."); }
+		if (!commentAttributes.body)
+			{throw new Meteor.Error(422, "Oops. Please finish writing your review!"); }
+		if (!commentAttributes.postId)
+			{throw new Meteor.Error(422, "You must review a doctor."); }
+
+		comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+		//update post with #of comments
+		Posts.update(comment.postId, {$inc: {commentsCount: 1}});
+		
+		//create comment, save _id
+		comment._id = Comments.insert(comment);
+		//create notification when user's post has been commented upon or additional comments have been posted
+		createCommentNotification(comment);
+		return comment._id;
+		//return Comments.insert(comment);
+	}
+});
---
+++ collections/posts.js
@@ -0,0 +1,45 @@
+Posts = new Meteor.Collection('posts');
+
+Posts.allow({
+	update: ownsDocument,
+	remove: ownsDocument
+	
+});
+
+Posts.deny({
+	update: function(userId, post, fieldNames) {
+		//may only edit the following fields:
+		return (_.without(fieldNames, 'url', 'title').length > 0);
+	}
+});
+
+Meteor.methods({
+	post: function(postAttributes) {
+		var user = Meteor.user(),
+			postWithSameLink = Posts.findOne({url: postAttributes.url});
+	//ensure the user is logged in
+	if (!user)
+		throw new Meteor.Error(401, "Sorry! You need to login to review a doctor.");
+
+	//post must have a Dr's name
+	if (!postAttributes.title)
+		throw new Meteor.Error(422, "Please add the name of the doctor you wish to review.");
+
+	//no repeats - doc name
+	//!!!!!!!!!! will need more work in future. find a way to differentiate amongst doctors
+	if (postAttributes.url && postWithSameLink) {
+		throw new Meteor.Error(302, "This doctor already exists in our system! Please add your review under this doctor's page.", postWithSameLink._id);
+}
+	//pick out whitelisted keys
+	var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+		userId: user._id,
+		author: user.username,
+		submitted: new Date().getTime(),
+		commentsCount:0
+	});
+
+	var postId = Posts.insert(post);
+
+	return postId;
+	}
+});
